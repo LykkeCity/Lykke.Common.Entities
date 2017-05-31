5c23c3727ac76706e818361579c1871dd836ba48
Anton Sivov <anton.sivov@inbox.ru>
2017-05-31 17:12:37 +0300

    Added MerchantAuthRequest

---
+++ src/Security/MerchantAuthRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Lykke.Common.Entities.Security
+{
+    public class MerchantAuthRequest
+    {
+        public string MerchantId { get; set; }
+        public string StringToSign { get; set; }
+        public string Sign { get; set; }
+    }
+}
