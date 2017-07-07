100a0c32a163ca7e12ca088471bda3e2ef2d0b73
Anton Sivov <anton.sivov@inbox.ru>
2017-07-07 11:21:28 +0300

    Added models for wallet

---
+++ src/Wallets/WalletModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lykke.Common.Entities.Wallets
+{
+    public class WalletModel
+    {
+        public string Address { get; set; }
+
+        public double AmountChange { get; set; }
+
+        public string TransactionId { get; set; }
+    }
+}
---
+++ src/Wallets/WalletMqModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lykke.Common.Entities.Wallets
+{
+    public class WalletMqModel
+    {
+        public List<WalletModel> Wallets { get; set; }
+
+        public WalletMqModel()
+        {
+            Wallets = new List<WalletModel>();
+        }
+    }
+}
