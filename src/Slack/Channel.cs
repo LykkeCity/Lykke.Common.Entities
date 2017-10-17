853dac75c0602f9e0f50f4a0bd28b6032e892993
Mikhail Musin <mikhail.musin@gmail.com>
2017-10-17 11:02:03 +0300

    slack

---
+++ src/Slack/Channel.cs
@@ -0,0 +1,8 @@
+namespace Lykke.Contracts.Slack
+{
+    public class Channel
+    {
+        public string Type { get; set; }
+        public string WebHookUrl { get; set; }
+    }
+}
---
+++ src/Slack/SlackIntegrationSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Lykke.Contracts.Slack
+{
+    public class SlackIntegrationSettings
+    {
+        public string Env { get; set; }
+        public Channel[] Channels { get; set; }
+    }
+}
