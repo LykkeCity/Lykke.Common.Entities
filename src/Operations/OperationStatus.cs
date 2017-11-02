05398df033e010b9a4376b83c5f02e0e5f48d56e
Mikhail Musin <mikhail.musin@gmail.com>
2017-11-02 12:46:27 +0300

    canceled status + flags

--- src/Operations/OperationStatus.cs
+++ src/Operations/OperationStatus.cs
@@ -1,11 +1,15 @@
-﻿namespace Lykke.Contracts.Operations
+﻿using System;
+
+namespace Lykke.Contracts.Operations
 {
+    [Flags]
     public enum OperationStatus
     {
         Created,
         Accepted,
         Confirmed,
         Completed,
+        Canceled,
         Failed
     }
 }
